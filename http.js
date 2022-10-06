ef3537ee44cb0527a650589b9b724d9361242f5e
paobuji <46154887+paobuji@users.noreply.github.com>
2022-10-06 10:57:52 +0800

    2 files modified, 1 added

--- http.js
+++ http.js
@@ -1,22 +1,26 @@
 // 导入http模块:
-var http = require('http');
+const fs = require('fs');
+const http = require('http');
+const path = require('path');
 // 创建http server，并传入回调函数:
-var server = http.createServer(function (request, response) {
-  // 回调函数接收request和response对象,
+var server = http.createServer((req, res) => {
+  // 回调函数接收req和res对象,
   // 获得HTTP请求的method和url:
-  console.log(request.method + ': ' + request.url);
-  // 将HTTP响应200写入response, 同时设置Content-Type: text/html:
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  // 将HTTP响应的HTML内容写入response:
-  response.end('Hello world!');
+  console.log(req.method + ': ' + req.url);
+  // 将HTTP响应200写入res, 同时设置Content-Type: text/html:
+  res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' });
+  // 将HTTP响应的HTML内容写入res:
+  const fpath = path.join(__dirname, '/1.html');
+  fs.readFile(fpath, 'utf-8', (err, data) => {
+    if (err) {
+      return err;
+    } else {
+      res.end(data);
+    }
+  });
 });
 
 // 让服务器监听8080端口:
 server.listen(8080);
 
-console.log('Server is running at http://127.0.0.1:8080/');
-
-// 作者：九旬
-// 链接：https://juejin.cn/post/6844903737522126861
-// 来源：稀土掘金
-// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+// console.log('Server is running at http://127.0.0.1:8080/');
