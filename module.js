ac2b77dd762ae0c82f3640bdd3f828d94b827c91
paobuji <46154887+paobuji@users.noreply.github.com>
2022-10-06 16:51:16 +0800

    1 file modified

--- module.js
+++ module.js
@@ -1,6 +1,14 @@
 exports.name = 'chp';
-module.exports.age = 20;
-
+exports.age = 20;
+exports.body = 'health';
 module.exports = {
   body: 'health',
 };
+
+exports = {
+  //这种写法是无效的
+  aaa: 'aaa',
+};
+
+// exports 和 module.exports 最终的结果是永远遵循module.exports = {} 的结果,
+// exports === module.exports === {}
